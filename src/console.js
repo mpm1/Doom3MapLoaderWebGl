98967e35a55b06cc8d48af5d45b8efecc22fc3c6
mpm1 <mpm1@ualberta.ca>
2019-05-02 21:34:30 -0600

    Added the ability to create console commands.

--- src/console.js
+++ src/console.js
@@ -1,12 +1,64 @@
-function Console(inputElement, outputElement){
-    this.output = $(outputElement);
-    this.input = $(inputElement);
+function ConsoleFunction(name, description, functionCall){
+    this.name = name;
+    this.description = description;
+    this.functionCall = functionCall;
+}
+ConsoleFunction.prototype.execute = function(parameters){
+    this.functionCall.apply(this, parameters);
+}
+
+function Console(inputElement, outputElement, submitButton){
+    this.init(inputElement, outputElement, submitButton);
 }
 {
+    var functions = {
+        "help" : new ConsoleFunction("help", "Lists all functions within the system.", function(){
+            for(var key in functions){
+                Console.current.writeLine(key + " - " + functions[key].description);
+            }
+        })
+    };
+    Console.current = null;
+
+    Console.prototype.init = function(inputElement, outputElement, submitButton){
+        this.output = $(outputElement);
+        this.input = $(inputElement);
+
+        var _this = this;
+        Console.current = this;
+
+        $(submitButton).click(function(ev){
+            ev.preventDefault();
+
+            var input = _this.input.val().trim();
+
+            if(input.length > 0){
+                var params = input.split(/\s+/);
+                _this.runFunction(params[0], params.slice[1]);
+            }
+        })
+    }
+
     Console.prototype.writeLine = function(text){
         var output = $("<p></p>");
         output.text(text);
         
         this.output.append(output);
     }
+
+    Console.prototype.addFunction = function(name, consoleFunction){
+        functions[name] = consoleFunction;
+    }
+
+    Console.prototype.runFunction = function(name, parameters){
+        var f = functions[name];
+
+        if(f){
+            f.execute(parameters);
+        }else{
+            this.writeLine("Function " + name + " does not exist.");
+        }
+    }
+
+
 }
--- src/doom3.js
+++ src/doom3.js
@@ -1,4 +1,4 @@
-function Game(canvasPath, consoleOutputPath, consoleInputPath){
+function Game(canvasPath, consoleOutputPath, consoleInputPath, consoleButtonPath){
     this.display = new Display($(canvasPath)[0]);
-    this.console = new Console($(consoleInputPath)[0], $(consoleOutputPath)[0]);
+    this.console = new Console($(consoleInputPath)[0], $(consoleOutputPath)[0], $(consoleButtonPath)[0]);
 }
